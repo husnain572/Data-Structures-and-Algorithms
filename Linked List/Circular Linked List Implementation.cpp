0986184da4885554e84835d978d17301728b4782
husnain572 <111906845+husnain572@users.noreply.github.com>
2025-05-25 13:03:30 +0500

    Circular Linked List Practise

---
+++ Linked List/Circular Linked List Implementation.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+    int data;
+    Node* next;
+
+public:
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+
+    friend class CircularList; // Allow CircularList to access private members
+};
+
+class CircularList {
+    Node* head;
+    Node* tail;
+
+public:
+    CircularList() {
+        head = tail = NULL;
+    }
+
+    void insertAtHead(int val) {
+        Node* newNode = new Node(val);
+        if (head == NULL) {
+            head = tail = newNode;
+            tail->next = head;
+        } else {
+            newNode->next = head;
+            head = newNode;
+            tail->next = head;
+        }
+    }
+
+    void insertAtTail(int val) {
+        Node* newNode = new Node(val);
+        if (tail == NULL) {
+            head = tail = newNode;
+            tail->next = head;
+        } else {
+            newNode->next = head;
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void deleteAtHead() {
+        if (head == NULL) return;
+
+        if (head == tail) {
+            delete head;
+            head = tail = NULL;
+        } else {
+            Node* temp = head;
+            head = head->next;
+            tail->next = head;
+            temp->next = NULL;
+            delete temp;
+        }
+    }
+
+    void deleteAtTail() {
+        if (head == NULL) return;
+
+        if (head == tail) {
+            delete head;
+            head = tail = NULL;
+        } else {
+            Node* temp = tail;
+            Node* prev = head;
+            while (prev->next != tail) {
+                prev = prev->next;
+            }
+            tail = prev;
+            tail->next = head;
+            temp->next = NULL;
+            delete temp;
+        }
+    }
+
+    void print() {
+        if (head == NULL) return;
+
+        Node* temp = head;
+        do {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        } while (temp != head);
+        cout << "HEAD" << endl; // indicate circular nature
+    }
+};
+
+int main() {
+    CircularList l;
+    l.insertAtHead(5);
+    l.insertAtHead(12);
+    l.insertAtHead(14);
+    l.insertAtTail(72);
+    l.deleteAtTail();
+    l.deleteAtHead();
+    l.print();
+    return 0;
+}
