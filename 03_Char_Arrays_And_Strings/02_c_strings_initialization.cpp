18e8a825657f051b5d8907bc8206391d89d14899
husnain572 <111906845+husnain572@users.noreply.github.com>
2026-05-19 12:44:33 +0500

    Add C-style string initialization example

---
+++ 03_Char_Arrays_And_Strings/02_c_strings_initialization.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char arr1[]="code";
+    char arr2[5]="code";
+    char arr3[]={'c','o','d','e','\0'};
+    char arr4[50]={'c','o','d','e','\0'};
+    cout<<arr3;
+    return 0;
+}
